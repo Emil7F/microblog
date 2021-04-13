de779cfd7d5fd6b92543ca2ac250f7a1e2914850
emil7f <emilfigzal@gmail.com>
2021-04-13 20:43:21 +0200

    basic security config

--- src/main/java/pl/emil7f/microblog/controller/WeatherController.java
+++ src/main/java/pl/emil7f/microblog/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package pl.emil7f.microblog.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,11 @@ public class WeatherController {
     }
 
     @GetMapping("/weather/{city}")
-    public WeatherDto getWeather(@PathVariable String city) {
-        return weatherService.getWeather(city);
+    public ResponseEntity<WeatherDto> getWeather(@PathVariable String city) {
+        WeatherDto  weather = weatherService.getWeather(city);
+        if(weather.getHumidity() == 0){
+         return    ResponseEntity.status(404).body(weather);
+        }
+        return   ResponseEntity.status(200).body(weather);
     }
 }
